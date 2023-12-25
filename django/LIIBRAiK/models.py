545287ea9cc75198061852fed7fea82943bc93eb
LazySlav <72216859+LazySlav@users.noreply.github.com>
2023-12-26 01:06:05 +0300

    wip models

--- django/LIIBRAiK/models.py
+++ django/LIIBRAiK/models.py
@@ -1,5 +1,5 @@
-from django.db.models.signals import pre_delete, post_delete
-import argon2
+import inspect
+from django.db.models.signals import pre_delete
 from django.db import models
 import uuid
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
@@ -17,73 +17,69 @@ Bullet-points:
 
 
 class UserManager(BaseUserManager):
-
     def __field_check(*fields):
         for field in fields:
             if field is None:
-                field_name = f"{field=}".split("=")[0]
-                raise ValidationError(f"{field_name} cannot be null")
+                raise ValidationError(
+                    f"Something went wrong when checking fields")
         return True
 
-    def create_user(self, id=None, library_card=None, name=None, surname=None, mail=None, role=None, password=None, salt=None, last_name=None):
+    def create_user(self, id=None, library_card=None, name=None, surname=None, mail=None, role=None, password=None, last_name=None):
 
         # how tf do you make proper error-handling without breaking the server?
         self.__field_check(id, library_card, name, surname,
-                           mail, role, password, salt, last_name)
+                           mail, role, password)
 
-        id = self.normalize_email(id)
-        user = self.model(
+        mail = self.normalize_email(mail)
+        user: User = self.model(
             id=id,
             library_card=library_card,
             name=name,
             surname=surname,
             mail=mail,
             role=role,
-            salt=salt,
             last_name=last_name
         )
-        user.password = argon2.hash(password + salt)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, id=None, library_card=None, name=None, surname=None, mail=None, role=None, password=None, salt=None, last_name=None):
+    def create_superuser(self, id=None, library_card=None, name=None, surname=None, mail=None, role=None, password=None, last_name=None):
 
         self.__field_check(id, library_card, name, surname,
-                           mail, role, password, salt, last_name)
+                           mail, role, password)
 
         id = self.normalize_email(id)
-        user = self.model(
+        user: User = self.model(
             id=id,
             library_card=library_card,
             name=name,
             surname=surname,
             mail=mail,
             role=role,
-            salt=salt,
             last_name=last_name
         )
-        user.password = argon2.hash_password(password + salt)
-        user.is_staff = True
-        user.is_superuser = True
+        user.role = "admin"
+        print(password)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
 
 class User(AbstractBaseUser):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    password = models.BinaryField()
-    salt = models.BinaryField()
-    library_card = models.TextField()
+    id = models.TextField(primary_key=True, default=uuid.uuid4)
+    library_card = models.TextField(default="default")
     name = models.TextField()
     surname = models.TextField()
     last_name = models.TextField(null=True, blank=True)
-    role = models.TextField()
+    role = models.TextField(default="student")
     mail = models.TextField()
     last_login = models.DateTimeField(blank=True, null=True)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'id'
+    REQUIRED_FIELDS = ["library_card", "name", "surname", "role", "mail"]
 
 
 class Author(models.Model):
@@ -91,16 +87,18 @@ class Author(models.Model):
         primary_key=True, default=uuid.uuid4)
     name = models.TextField()
 
-# questionable code
+# questionable code made for proper book authors deletions
 
 
 @receiver(pre_delete, sender=Author)
 def remove_author_from_books(sender, instance, **kwargs):
+    books_to_delete = []
     for book in instance.books.all():
         if book.authors.count() > 1:
             book.authors.remove(instance)
         else:
-            instance.books.filter(author__id=instance.author_id)
+            books_to_delete.append(book)
+    Book.objects.filter(id__in=[book.id for book in books_to_delete]).delete()
 
 
 class Publisher(models.Model):
@@ -116,7 +114,7 @@ class Book(models.Model):
     year = models.SmallIntegerField(null=True, blank=True)
     ISBN = models.TextField(null=True, blank=True)
     authors = models.ManyToManyField(
-        Author, related_name="books", null=True, through="BookAuthor")
+        Author, related_name="books", through="BookToAuthor")
     publisher = models.ForeignKey(
         Publisher, on_delete=models.CASCADE, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
@@ -129,9 +127,11 @@ class Book(models.Model):
         self.authors = self.authors.all()[:10]
         super().save(*args, **kwargs)
 
+# ethically questionable practices here, folks
 
-class BookAuthor(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.DO_NOTHING)
+
+class BookToAuthor(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
 
 
