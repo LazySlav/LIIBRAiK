04899339a1e333b11fe77e2bcf7055836530f026
LazySlav <72216859+LazySlav@users.noreply.github.com>
2024-01-10 00:51:21 +0300

    start.sh fixes

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+path=$(dirname "$0")
+# set -a: Mark variables and function which are modified or created for export to the environment of subsequent commands
 set -a;
-source .env;
+source $path/.env;
 set +a;
 if [ -n "$1" ]; then
-    python django/manage.py $1 $2 $3
+    python $path/django/manage.py $1 $2 $3
 else 
-    python django/manage.py runserver
+    python $path/django/manage.py runserver
 fi
 exit 0
