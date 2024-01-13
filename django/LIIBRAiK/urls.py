851e47cba535a858ecc83e2f37a3d340b8ac686e
LazySlav <72216859+LazySlav@users.noreply.github.com>
2024-01-14 00:18:49 +0300

    routing go vroom

--- django/LIIBRAiK/urls.py
+++ django/LIIBRAiK/urls.py
@@ -17,17 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from django.contrib.auth.views import LoginView
+
 from . import views
 # from .views import test
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'users', views.BookViewSet)
-router.register(r'users', views.AuthorViewSet)
-router.register(r'users', views.ReservationViewSet)
-router.register(r'users', views.PublisherViewSet)
+router.register(r'books', views.BookViewSet)
+router.register(r'authors', views.AuthorViewSet)
+router.register(r'reservations', views.ReservationViewSet)
+router.register(r'publishers', views.PublisherViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('auth/login/', LoginView.as_view(template_name='rest_framework/login.html'), name='login'),
+    # path('auth/logout/', views.LogoutView.as_view(), name='logout'),
 ]
 urlpatterns += router.urls
